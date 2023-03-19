d4232fc80e92f80e59e0fc8218095c2548425019
Cecilia Mary <ceciliamary78@gmail.com>
2023-03-18 22:42:55 -0300

    cv

---
+++ funciones.js
@@ -0,0 +1,4 @@
+<>
+<button type="button" onclick="document.getElementById('demo').style.display='block'">Email</button>
+<button type="button" onclick="document.getElementById('demo').style.display='block'">Telefono</button>
+</>
